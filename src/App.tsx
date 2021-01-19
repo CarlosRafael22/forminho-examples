7941dc5b3727be3ba2c7618077187cc0045ebb8d
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-19 12:59:35 -0300

    feat(app): Add initial example components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  SimpleForm,
+  FormWithAllFields
+} from './examples'
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h2>Simple Form</h2>
+      <SimpleForm />
+      <h2>Example with All Fields available</h2>
+      <FormWithAllFields />
+    </>
+  )
 }
 
 export default App;
